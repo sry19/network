62769571af61ea476366fad47c559a9325c0acbd
rebeccasun <sun.ruoy@husky.neu.edu>
2020-10-10 18:57:42 -0700

    refactor the codes

--- hw1/src/main/java/Handler.java
+++ hw1/src/main/java/Handler.java
@@ -7,7 +7,6 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Pattern;
 
 public class Handler implements Runnable {
 
@@ -25,62 +24,32 @@ public class Handler implements Runnable {
 
         InputStream inputStream = this.clientSocket.getInputStream();
 
-        byte[] result = new byte[0];
-        int i = 0;
-        byte[] tmp = new byte[16];
-        while((i = inputStream.read(tmp,0, 16))==16) {
-          byte[] newResult = new byte[result.length + i];
-          System.arraycopy(result, 0, newResult, 0, result.length);
-          System.arraycopy(tmp, 0, newResult, result.length, i);
-          result = newResult;
-        }
+        byte[] result = this.getInputStreamBytes(inputStream);
 
-        if (i != -1) {
-          byte[] newResult = new byte[result.length+i];
-          System.arraycopy(result,0,newResult,0,result.length);
-          System.arraycopy(tmp,0,newResult,result.length,i);
-          result = newResult;
-        }
-        List<String> expressions = new ArrayList<>();
         int idx = 0;
-        int l = (int)result[idx+1] & 0xFF;
-        l += ((int)result[idx] & 0xFF) << 8;
+        byte[] numberOfExpression = Arrays.copyOfRange(result, idx, idx + 2);
+        int l = this.convertBigEndianToInt(numberOfExpression);
+
         System.out.println(l);
 
-        byte[] output = new byte[2];
-        output[0] = (byte) (l >> 8 & 0xFF);
-        output[1] = (byte) (l & 0xFF);
+        byte[] output = this.convertIntToBigEndian(l);
 
         idx = idx + 2;
         while (l > 0) {
-          int lenOfExpression = (int) result[idx + 1] & 0xFF;
-          lenOfExpression += ((int) result[idx] & 0xFF) << 8;
+          byte[] lengthOfExpression = Arrays.copyOfRange(result, idx, idx + 2);
+          int lenOfExpression = this.convertBigEndianToInt(lengthOfExpression);
+
           idx += 2;
           byte[] expression = Arrays.copyOfRange(result, idx, idx + lenOfExpression);
           String stringOfExpression = new String(expression);
-          int answer = 0;
-          int tmpAnswer = 0;
-          int sign = 1;
-          for (int x = 0; x < stringOfExpression.length(); x++) {
-            if (isDigit(stringOfExpression.charAt(x))) {
-              tmpAnswer = tmpAnswer * 10 + Integer.parseInt(String.valueOf(stringOfExpression.charAt(x)));
-            } else if (stringOfExpression.charAt(x) == '-') {
-              answer += sign*tmpAnswer;
-              tmpAnswer = 0;
-              sign = -1;
-            } else {
-              answer += sign*tmpAnswer;
-              tmpAnswer = 0;
-              sign = 1;
-            }
-          }
-          answer += sign*tmpAnswer;
-          System.out.println(answer);
+
+          int answer = this.eval(stringOfExpression);
+
           String stringAnswer = String.valueOf(answer);
           int lengthOfAnswer = stringAnswer.length();
-          byte[] bytesOfLengthOfAnswer = new byte[2];
-          bytesOfLengthOfAnswer[0] = (byte) (lengthOfAnswer >> 8 & 0xFF);
-          bytesOfLengthOfAnswer[1] = (byte) (lengthOfAnswer & 0xFF);
+
+
+          byte[] bytesOfLengthOfAnswer = this.convertIntToBigEndian(lengthOfAnswer);
 
           byte[] answerToBytes = stringAnswer.getBytes();
           byte[] newOutput = new byte[output.length+bytesOfLengthOfAnswer.length+answerToBytes.length];
@@ -101,5 +70,58 @@ public class Handler implements Runnable {
       } catch (IOException ex) {
       ex.printStackTrace();
     }
+  }
+
+  private int convertBigEndianToInt(byte[] bigEndian) {
+    int l = (int)bigEndian[1] & 0xFF;
+    l += ((int)bigEndian[0] & 0xFF) << 8;
+    return l;
+  }
+
+  private byte[] convertIntToBigEndian(int l) {
+    byte[] output = new byte[2];
+    output[0] = (byte) (l >> 8 & 0xFF);
+    output[1] = (byte) (l & 0xFF);
+    return output;
+  }
+
+  private byte[] getInputStreamBytes(InputStream inputStream) throws IOException {
+    byte[] result = new byte[0];
+    int i = 0;
+    byte[] tmp = new byte[16];
+    while((i = inputStream.read(tmp,0, 16))==16) {
+      byte[] newResult = new byte[result.length + i];
+      System.arraycopy(result, 0, newResult, 0, result.length);
+      System.arraycopy(tmp, 0, newResult, result.length, i);
+      result = newResult;
+    }
+    if (i != -1) {
+      byte[] newResult = new byte[result.length+i];
+      System.arraycopy(result,0,newResult,0,result.length);
+      System.arraycopy(tmp,0,newResult,result.length,i);
+      result = newResult;
+    }
+    return result;
+  }
+
+  private int eval(String stringOfExpression) {
+    int answer = 0;
+    int tmpAnswer = 0;
+    int sign = 1;
+    for (int x = 0; x < stringOfExpression.length(); x++) {
+      if (isDigit(stringOfExpression.charAt(x))) {
+        tmpAnswer = tmpAnswer * 10 + Integer.parseInt(String.valueOf(stringOfExpression.charAt(x)));
+      } else if (stringOfExpression.charAt(x) == '-') {
+        answer += sign*tmpAnswer;
+        tmpAnswer = 0;
+        sign = -1;
+      } else {
+        answer += sign*tmpAnswer;
+        tmpAnswer = 0;
+        sign = 1;
+      }
+    }
+    answer += sign*tmpAnswer;
+    return answer;
   }
 }
